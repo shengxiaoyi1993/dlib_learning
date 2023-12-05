b3d4d516cf402b5adb993bf3efb48db31d763a3c
qc_da4e05189d814e28850771a1831dbc61 <shengxiaoyi2014@icloud.com>
2023-12-05 18:04:17 +0800

    add CardFinder

---
+++ CardFinder/demos/call_lib/src/main.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <opencv_basic/opencv_basic.h>
+#include <opencv2/opencv.hpp>
+
+
+
+/// 经过多次手动标记,可以将所有图案添加到标准库
+
+/// 目前要做的是将分割出的图形与已有图片比较,若无匹配,则
+
+double getHVRate(const cv::Mat &mat)
+{
+  if (mat.empty()) {
+    return -1;
+  }
+
+  if (mat.type()!= CV_8U) {
+
+  }
+
+  double sum_h=0;
+  for(int r=0;r<mat.rows;r++)
+  {
+    for(int c=0;c<mat.cols;c++)
+    {
+      sum_h+=(int)mat.at<uchar>(r,c);
+    }
+
+  }
+
+
+}
+
+
+int main(int argc, char *argv[])
+{
+
+  return 0;
+}
+
+
+//double getHVRate(const cv::Mat &mat)
+//{
+
+//}
